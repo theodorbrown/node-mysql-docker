296fc8a7e8fd66d3a835b9a773a664cc78dc5e9e
theodorbrown <theodor.brown01@gmail.com>
2022-09-30 12:03:57 +0200

    upload

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express, {Application, Request, Response} from "express";
+import ip from 'ip';
+import cors from 'cors';
+import {Code} from "./enum/status.enum";
+import {HttpResponse} from "./domain/response";
+import {Status} from "./enum/code.enum";
+import patientRoutes from "./routes/patient.routes";
+
+export class App {
+    private readonly app: Application;
+    private readonly APPLICATION_RUNNING: string = 'Application is running on:';
+    private readonly ROUTE_NOT_FOUND: string = 'Route not found ! Route doesn\'t exist on the server';
+
+    constructor(private readonly port: (string | number) = process.env.SERVER_PORT || 3000) {
+        this.app = express();
+        this.middleWare();
+        this.routes();
+    }
+
+    listen(): void {
+        this.app.listen(this.port);
+        console.info(`${this.APPLICATION_RUNNING} ${ip.address()}:${this.port}`)
+    }
+
+    private middleWare(): void {
+        this.app.use(cors({
+            origin: '*'
+        }));
+        this.app.use(express.json());
+    }
+
+    private routes(): void {
+        this.app.use('/patients', patientRoutes);
+
+        this.app.get('/', (_: Request, res: Response) => {
+            res.status(Code.OK).send(new HttpResponse(Code.OK, Status.OK, 'Welcome to the Patients API'))
+        });
+
+        this.app.all('/*', (_: Request, res: Response) => {
+            res.status(Code.NOT_FOUND).send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, this.ROUTE_NOT_FOUND))
+        });
+    }
+}
---
+++ src/interface/patient.ts
@@ -0,0 +1,10 @@
+export interface Patient {
+    first_name: string,
+    last_name: string,
+    email: string,
+    address: string,
+    diagnosis: string,
+    phone: string
+    status: string,
+    image_url: string
+}
